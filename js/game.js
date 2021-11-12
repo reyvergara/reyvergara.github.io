36be8ebdbebe903ab6d7343b81cd873a1ce321b8
reyjvergara <rey.j.vergara@gmail.com>
2021-11-12 09:25:25 -0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -118,7 +118,7 @@ var Unit = new Phaser.Class({
         this.scene.events.emit("Message", this.type + " attacks " + target.type + " for " + this.damage + " damage");
     },
     takeDamage: function(damage) {
-        console.log(this.type + "Took damage");
+        console.log(this.damage + "Took damage");
         this.hp -= damage;
         if(this.hp <= 0) {
             this.hp = 0;
